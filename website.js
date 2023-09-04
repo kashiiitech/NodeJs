bf2afebeb57b2b4ce0e33fb8cc2b0534d4473bfe
kashiiitech <p200648@nu.edu.pk>
2023-09-04 20:06:58 +0500

    expressjs

--- website.js
+++ website.js
@@ -1,6 +1,7 @@
+const fs = require('fs');
 const http = require('http');
 
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 9001;
 
 const server = http.createServer((req, res) => {
   // console.log(req.url);
@@ -9,12 +10,19 @@ const server = http.createServer((req, res) => {
 
   if(req.url == '/') {
     res.statusCode = 200;
-    res.end('<h1> This is Kashiiitech...</h1><p>Hey this is the way to rock the world!!</p>');
+    const data = fs.readFileSync('index.html');
+    res.end(data.toString());
   }
   else if (req.url == '/about') {
     res.statusCode = 200;
     res.end('<h1> This is About Kashiiitech...</h1><p>Hey this is the way to rock the world!! In the About section</p>');
   }
+  else if (req.url == '/hello') {
+    res.statusCode = 200;
+    res.end('<h1> This is Kashiiitech...</h1><p>Hey this is the way to rock the world!!</p>');
+    
+  }
+
 
   else {
     res.statusCode = 404;
